ac17f79081777676fe21b308599525323310361a
Moonjoon Kim <doublemoon@kakao.com>
2022-04-18 18:36:27 +0900

    2022-04-18

--- Ex/EX001.java
+++ Ex/EX001.java
@@ -5,14 +5,14 @@ import java.util.Scanner;
 public class EX001 {
 
 	public static void main(String[] args) {
-		System.out.println("\\    /\\\n )  ( ')\n(  /  )\n \\(__)|");
+		Scanner sc = new Scanner(System.in);
+		/*System.out.println("\\    /\\\n )  ( ')\n(  /  )\n \\(__)|");
 		
 		System.out.println();
 		
 		System.out.println("|\\_/|\n|q p|   /}\n( 0 )\"\"\"\\\n|\"^\"`    |\n||_/=\\\\__|");
 		
 		System.out.println();
-		Scanner sc = new Scanner(System.in);
 		
 		double A;
 		double B;
@@ -24,12 +24,24 @@ public class EX001 {
 		
 		System.out.println();
 		
-		System.out.println("joonas??!");
+		String id = sc.next();
+		System.out.println(id + "??!");
 		
+		int y = sc.nextInt();
+		System.out.println(y - 543);
 		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = ((b%100)%10) * a;
+		int d = ((b%100)/10 * a);
+		int e = ((b/100) * a);
+		int f = c + d * 10 + e * 100;
 		
-		
-		
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+		*/
 		
 		
 		
